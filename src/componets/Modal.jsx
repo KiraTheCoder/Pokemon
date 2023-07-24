dea4fb16df6f9e709bb83fdbc1f8f180b2391e69
Kiran <kiran@kali.kali>
2023-07-24 18:06:15 +0530

    static madal ready

--- src/componets/Modal.jsx
+++ src/componets/Modal.jsx
@@ -20,49 +20,86 @@ const Modal = ({ pokemonName }) => {
 
     return (
         <div className="modal fade" id="pokemonModal" tabindex="-1" aria-labelledby="PokemonModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <div className="modal-header">
+            <div className="modal-dialog modal-dialog-centered ">
+                <div className="modal-content modalContainer">
+                    <div className="modal-header border-0">
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
 
 
-                    <div className="modal-body p-0">
-                        <div class="container-fluid d-flex justify-content-center ">
-                            <div class="row">
+                    <div className="modal-body p-0 ">
+                        <div class=" container-fluid d-flex justify-content-center ">
+                            {/* 
 
 
                                 {pokemonFound
                                     ?
-                                    <div className="col-12 position-relative">
-                                        <div className="Pokédex text-center">
-
-                                            {/* <img className="modalImage" src={pokemonData.sprites.other.dream_world.front_default} alt="Pokemon" />
-                                                                                        
-                                            <h1>{pokemonData.name}</h1>
-                                            <span>
-                                                <h2>{pokemonData.weight}</h2>
-                                                <h2>WEIGHT</h2>
-                                            </span>
-                                            <span>
-                                                <h2>{pokemonData.types[0].type.name}</h2>
-                                                <h2>TYPE</h2>
-                                            </span>
-                                            <span>
-                                                <h2>{pokemonData.height}</h2>
-                                                <h2>HEIGHT</h2>
-                                            </span>
-                                            {console.log(pokemonData.sprites.other.dream_world.front_default)} */}
-
-                                        </div>
-                                    </div>
-
                                     :
 
                                     <h4>Pokémon Not Found</h4>
                                 }
 
+                            */}
+
+
+
+
+                            <div class="Pokédex">
+
+                                <div class="item item1">
+                                    <img class="modalImage"
+                                        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+                                        alt="Pokemon" />
+                                </div>
+                                <div class="item item2">
+                                    <h1>bulbasaur</h1>
+
+                                </div>
+                                <div class="item item3">
+                                    <h2>#1</h2>
+
+                                </div>
+                                <div class="item item4">
+                                    <h2>ABILITY</h2>
+
+                                </div>
+                                <div class="item item5">
+                                    <h3>overgrow</h3>
+                                    <h3>chlorophyll</h3>
+                                </div>
+                                <div class="item item6">
+                                    <h2>TYPE</h2>
+                                </div>
+                                <div class="item item7">
+                                    <h3>grass</h3>
+                                    <h3>poison</h3>
+                                </div>
+                                <div class="item item8">
+                                    <h2>WEIGHT</h2>
+                                    <h2>HEIGHT</h2>
+                                </div>
+                                <div class="item item9">
+                                    <h3>6.9kg</h3>
+                                    <h3>0.7m</h3>
+                                </div>
+                                <div class="item item10">
+                                    <h2>STATS</h2>
+                                </div>
+                                <div class="item11">
+                                    <p>HP : 45</p>
+                                    <p>ATTACK : 49</p>
+                                    <p>DEFENSE :49</p>
+                                </div>
+                                <div class="item12">
+                                    <p>SPECIAL-ATTACK : 65</p>
+                                    <p>SPECIAL-DEFENSE : 65</p>
+                                    <p>SPEED : 45</p>
+                                </div>
                             </div>
+
+
+
+
                         </div>
 
 
@@ -76,33 +113,4 @@ const Modal = ({ pokemonName }) => {
 
 export default Modal
 
-
-    // {
-    //     <h1>{data.name}</h1>
-    //     <img src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" />
-    //     <div className="abilities">
-    //         {
-    //             data.abilities.map(poke=>{
-    //                 return(
-    //                     <>
-    //                      <div className="group">
-    //                         <h2>{poke.ability.name}</h2>
-    //                     </div>
-    //                     </>
-    //                 )
-    //             })
-    //         }
-    //     </div>
-    //     <div className="base-stat">
-    //         {
-    //             data.stats.map(poke=>{
-    //                 return(
-    //                     <>
-    //                         <h3>{poke.stat.name}:{poke.base_stat}</h3>
-    //                     </>
-    //                 )
-    //             })
-    //         }
-    //     </div>
-    // }
 
